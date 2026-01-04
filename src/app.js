2991e2d6c2c9edbf9484a989895e8c8e17f0fd83
matanmalka1 <matan1391@gmail.com>
2026-01-04 12:21:07 +0200

    Add Input Sanitization with express-mongo-sanitize

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
 import { globalRateLimiter } from "./middlewares/rateLimiter.js";
 import { requestLogger } from "./middlewares/requestLogger.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
@@ -22,6 +23,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(mongoSanitize());
 
 app.use(requestLogger);
 
